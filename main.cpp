f67a8b1f65b45f7b3c09d1bf21a02d6573ba365c
RobinsAviary <robinsaviary@gmail.com>
2025-05-03 09:47:15 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <filesystem>
+#include <fstream>
+
+std::string code;
+bool terminalActive = true;
+
+int acc = 0;
+
+char argPrefix = '-';
+bool verboseOutput = false;
+bool mathFix = false;
+
+void accCheck() {
+	if (!mathFix) {
+		if (acc == -1 || acc == 256) acc = 0;
+	}
+	else {
+		// A fix for deadfish's odd implementation that better matches specifications.
+		while (acc < 0) {
+			acc += 256;
+		}
+		while (acc > 255) {
+			acc -= 256;
+		}
+	}
+}
+
+void opI() {
+	acc++;
+}
+
+void opD() {
+	acc--;
+}
+
+void opS() {
+	acc *= acc;
+}
+
+void opO() {
+	std::cout << acc << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+	bool fileFound = false;
+
+	for (int i = 0; i < argc; i++) {
+		std::string_view currentArg = argv[i];
+
+		if (currentArg.starts_with(argPrefix)) {
+			if (currentArg == argPrefix + "v" || currentArg == argPrefix + "verbose") {
+				verboseOutput = true;
+			}
+			else if (currentArg == argPrefix + "f" || currentArg == argPrefix + "fix") {
+				mathFix = true;
+			}
+		}
+
+		if (verboseOutput) std::cout << currentArg << std::endl;
+		
+		if (currentArg.ends_with(".df")) {
+			if (std::filesystem::exists(currentArg)) {
+				std::fstream file(static_cast<std::string>(currentArg));
+
+				std::string line;
+
+				while (std::getline(file, line)) {
+					code += line;
+					if (verboseOutput) std::cout << line << std::endl;
+				}
+
+				fileFound = true;
+				terminalActive = false;
+				i = argc;
+			};
+
+			//std::cout << argv[i] << std::endl;
+		}
+	}
+
+	// File-based interpreter. Slightly faster.
+	if (fileFound) {
+		std::ofstream log("log.txt");
+
+		for (char op : code) {
+			if (op == 'i') opI();
+			else if (op == 'd') opD();
+			else if (op == 's') opS();
+			else if (op == 'o') {
+				opO();
+				log << acc;
+			}
+			else if (op == 'h') break;
+
+			accCheck();
+		}
+
+		log.close();
+	}
+
+	// Terminal interpreter.
+	while (terminalActive) {
+		std::cout << ">> ";
+		std::string typedCode;
+		std::cin >> typedCode;
+
+		for (int i = 0; i < typedCode.size(); i++) {
+			char op = typedCode[i];
+
+			if (op == 'i') opI();
+			else if (op == 'd') opD();
+			else if (op == 's') opS();
+			else if (op == 'o') opO();
+
+			accCheck();
+		}
+	}
+	
+	return 0;
+}
